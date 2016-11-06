7cbacf76257f2d3dd96582b8dc08734c690a25ca
saikiran949 <saikiran9497@gmail.com>
2016-11-06 18:40:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ var articleone={
         <p>
             IIT Madras  is a residential  institute  that occupies a 2.5 square kilometers  campus that  was formerly part  of the adjoining Guindy National Park. The institute has nearly 550 faculty, 8,000 students and 1,250 administrative and supporting staff. Growing ever since it obtained its charter from the Indian Parliament in 1961, much of the campus is a protected forest, carved out of the Guindy National Park, home to large numbers of chital (spotted deer), black buck, monkeys, and other rare wildlife. A natural lake, deepened in 1988 and 2003, drains most of its rainwater.
 
-        </p>`
+        </p>
+        <p>
+         Internationally, IIT Madras was ranked #254 in the QS World University Rankings of 2015,[30] 56 in the QS Asian University Rankings and 20 in the QS BRICS University Rankings of 2015.[31]
+
+            The Times Higher Education World Rankings ranked it at 78 and 44 in Asia and BRICS countries respectively in 2015.
+
+            In India, National Institutional Ranking Framework has ranked it No.1 Engineering Institution in 2016.[40]
+</p>`
+
             
 };
 function createTemplate (data) {
