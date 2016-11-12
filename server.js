0071ba9fffe544cb63806ea3644a728d87548e43
saikiran949 <saikiran9497@gmail.com>
2016-11-12 18:54:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,12 +73,12 @@ var articles={
 
         </p>
         <p>
-         As part of Nehru's dream for a free self-sufficient India, the institute was established to train scientists and engineers after India attained independence in 1947. It shares its organisational structure and undergraduate admission process with sister IITs. The students and alumni of IIT Kharagpur are informally referred to as KGPians. Among all IITs, IIT Kharagpur has the largest campus (2,100 acres),[4] the most departments, and the highest student enrollment. IIT Kharagpur is known for its festivals: Spring Fest (Social and Cultural Festival) and Kshitij (Asia's largest Techno-Management Festival).
+         As part of Nehru's dream for a free self-sufficient India, the institute was established to train scientists and engineers after India attained independence in 1947. It shares its organisational structure and undergraduate admission process with sister IITs. The students and alumni of IIT Kharagpur are informally referred to as KGPians. Among all IITs, IIT Kharagpur has the largest campus (2,100 acres), the most departments, and the highest student enrollment. IIT Kharagpur is known for its festivals: Spring Fest (Social and Cultural Festival) and Kshitij (Asia's largest Techno-Management Festival).
 
 
         </p>
         <p>
-        Internationally, IIT Kharagpur is ranked 286 in the QS World University Rankings (Quacquarelli Symonds) of 2015[55][45] and 60 in the QS Asian University Rankings of 2014.[56][57][46] IIT Kharagpur is ranked 3 in National Institutional Ranking Framework published by Ministry of Human Resource Development in April 2016.[58][59]
+        Internationally, IIT Kharagpur is ranked 286 in the QS World University Rankings (Quacquarelli Symonds) of 2015 and 60 in the QS Asian University Rankings of 2014.IIT Kharagpur is ranked 3 in National Institutional Ranking Framework published by Ministry of Human Resource Development in April 2016.
        </p>`
 },
     'iitgandhinagar':{
@@ -86,13 +86,13 @@ var articles={
         heading:'ABOUT IIT-GANDHINAGAR',
         content:
         `<p>
-        Indian Institute of Technology Gandhinagar (also known as IIT Gandhinagar and IITGN), located in Gandhinagar, Gujarat, India, is a public institute of higher education declared to be an Institute of National Importance by the Government of India.[1]
+        Indian Institute of Technology Gandhinagar (also known as IIT Gandhinagar and IITGN), located in Gandhinagar, Gujarat, India, is a public institute of higher education declared to be an Institute of National Importance by the Government of India.
         </p>
         <p>
-        The permanent campus of IIT Gandhinagar, on the banks of the Sabarmati River near Palaj village, is under construction and is nearing completion.[8] In 2011, in his inaugural speech for the Amalthea technology summit, then Gujarat Chief Minister Narendra Modi spoke of the land for the new permanent campus, saying, "[the] [s]tate government has decided to give [...] land on 99 year lease with a token amount of just one rupee for setting up a campus of IIT-Gandhinagar."[9] The institute took possession of over 400 acres of land in August 2012 and the classes and other activities were started on the new campus from July 2015. The institute will function as a fully residential campus and will be constructed in phases. The campus will be run on solar power. It is envisioned to be a pollution-free, green zone.[10]
+        The permanent campus of IIT Gandhinagar, on the banks of the Sabarmati River near Palaj village, is under construction and is nearing completion. In 2011, in his inaugural speech for the Amalthea technology summit, then Gujarat Chief Minister Narendra Modi spoke of the land for the new permanent campus, saying, "[the] [s]tate government has decided to give [...] land on 99 year lease with a token amount of just one rupee for setting up a campus of IIT-Gandhinagar."[9] The institute took possession of over 400 acres of land in August 2012 and the classes and other activities were started on the new campus from July 2015. The institute will function as a fully residential campus and will be constructed in phases. The campus will be run on solar power. It is envisioned to be a pollution-free, green zone.
        </p>
         <p>
-        In 2011, IIT Gandhinagar and the Physical Research Laboratory signed a Memorandum of Understanding (MoU) that ensures research collaborations and exchanges between the two institutions at the level of advanced Ph.D. programs in theoretical physics, astro-physics, and atmospheric sciences.[36] The collaboration facilitates exchange of research ideas between Ph.D. students and the faculties of both institutions.
+        In 2011, IIT Gandhinagar and the Physical Research Laboratory signed a Memorandum of Understanding (MoU) that ensures research collaborations and exchanges between the two institutions at the level of advanced Ph.D. programs in theoretical physics, astro-physics, and atmospheric sciences. The collaboration facilitates exchange of research ideas between Ph.D. students and the faculties of both institutions.
 
          </p>
         <p>
@@ -108,13 +108,13 @@ var articles={
         Delhi Technological University's departments of Chemical Technology and Textile Technology were shifted out en-block to mark the beginning of IIT Delhi at its campus in Hauz Khas. The Delhi Technological University is thus the mother institution of IIT Delhi. The school, originally named the College of Engineering & Technology, was registered as a Society on 14 June 1960, and classes began in August 1961. The school was later accorded the status of a University and was renamed as Indian Institute of Technology Delhi.
         </p>
         <p>
-        IIT Delhi is located in Hauz Khas, South Delhi. The campus of 325 acres (132 ha) is surrounded by the beautiful Hauz Khas area and monuments such as the Qutub Minar and Lotus Temple.[3] The campus is also close to other educational institutions such as the Jawaharlal Nehru University, All India Institute of Medical Sciences, National Institute of Fashion Technology, National Council of Educational Research and Training (NCERT) and Indian Statistical Institute.
+        IIT Delhi is located in Hauz Khas, South Delhi. The campus of 325 acres (132 ha) is surrounded by the beautiful Hauz Khas area and monuments such as the Qutub Minar and Lotus Temple. The campus is also close to other educational institutions such as the Jawaharlal Nehru University, All India Institute of Medical Sciences, National Institute of Fashion Technology, National Council of Educational Research and Training (NCERT) and Indian Statistical Institute.
         </p>
         <p>
-        Internationally, IIT Delhi was ranked #185 in the QS World University Rankings in 2016[2] and at 38 in the QS Asian Rankings of 2014.[44][45] IIT Delhi has responded to being ranked 176 on the QS Worldwide University Rankings, with an appeal for more "investment in - and support for - the faculty."[46]
+        Internationally, IIT Delhi was ranked #185 in the QS World University Rankings in 2016 and at 38 in the QS Asian Rankings of 2014. IIT Delhi has responded to being ranked 176 on the QS Worldwide University Rankings, with an appeal for more "investment in - and support for - the faculty."
         </p>
         <p>
-        In 2014, the University ranked 4th in top higher education institutions in India, 59th in top 100 higher education Institutes in Asia.[47] In 2016, it has been ranked 5 in Careers360 Top Universities in India 2016[48] and ranks at 56th in BRICS and Emerging Economies[49] by the Times Higher Education World University Rankings [50]
+        In 2014, the University ranked 4th in top higher education institutions in India, 59th in top 100 higher education Institutes in Asia. In 2016, it has been ranked 5 in Careers360 Top Universities in India 2016 and ranks at 56th in BRICS and Emerging Economies by the Times Higher Education World University Rankings
         </p>`
 },
     'iitroorke':{
@@ -142,7 +142,7 @@ var articles={
         Indian Institute of Technology Guwahati (IIT Guwahati, IITG) is a public institution established by the Government of India, located in Guwahati, in the state of Assam in India. It is the sixth Indian Institute of Technology established in India.
         </p>
         <p>
-        The history of IIT Guwahati traces its roots to the 1985 Assam Accord[6] signed between the All Assam Students Union and the Government of India, which mentions the general improvement in education facilities in Assam and specifically the setting up of an IIT.
+        The history of IIT Guwahati traces its roots to the 1985 Assam Accord signed between the All Assam Students Union and the Government of India, which mentions the general improvement in education facilities in Assam and specifically the setting up of an IIT.
        </p>
         <p>
         The campus of IIT Guwahati is on the northern banks of Brahmaputra and abuts the North Guwahati town of Amingaon, pincode-781039. It is considered as the most beautiful campus in India.
@@ -150,7 +150,7 @@ var articles={
         
 
         <p>
-        Internationally, IIT Guwahati was ranked 451-460 in the QS World University Ranking of 2015[11] and 89 in the QS Asian University Rankings of 2012.[12] In 2016 QS World Universities Rankings by subject, Electronics & Electrical Engineering department is ranked 201-250,[17] Mechanical department is ranked 201-300,[18] Chemistry department is ranked 301-400 [19] and Computer Science department is ranked 401-500.[20] In India, among engineering colleges, it ranked 10 by India Today in 2012,[13] 8 by Outlook India in 2012[14] and 7 by Dataquest in 2011.[15] In the Mint Government Colleges survey of 2009 it ranked 6.[21] The Chemical Engineering department was ranked 101-150 in the QS World University Rankings by Subject 2012.[22] In 2014, IIT Guwahati has been ranked 87 in '100 Top Global Universities Under 50 Years' by the Times Higher Education World University Rankings, bringing India in the list for the very first time.[23] In 2015, IIT Guwahati has been ranked 61-70 in 'QS University Rankings: Top 50 Under 50' by the QS World University Rankings.[24] and rated AAAA+ by Careers360.
+        Internationally, IIT Guwahati was ranked 451-460 in the QS World University Ranking of 2015 and 89 in the QS Asian University Rankings of 2012. In 2016 QS World Universities Rankings by subject, Electronics & Electrical Engineering department is ranked 201-250, Mechanical department is ranked 201-300, Chemistry department is ranked 301-400  and Computer Science department is ranked 401-500. In India, among engineering colleges, it ranked 10 by India Today in 2012, 8 by Outlook India in 2012[14] and 7 by Dataquest in 2011. In the Mint Government Colleges survey of 2009 it ranked 6. The Chemical Engineering department was ranked 101-150 in the QS World University Rankings by Subject 2012. In 2014, IIT Guwahati has been ranked 87 in '100 Top Global Universities Under 50 Years' by the Times Higher Education World University Rankings, bringing India in the list for the very first time. In 2015, IIT Guwahati has been ranked 61-70 in 'QS University Rankings: Top 50 Under 50' by the QS World University Rankings. and rated AAAA+ by Careers360.
          
             
 </p>`
