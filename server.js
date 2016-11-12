03eda5613809e1ccbf2349dde881ef1140cf3aa0
saikiran949 <saikiran9497@gmail.com>
2016-11-12 16:57:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,27 @@ var articles={
             In India, National Institutional Ranking Framework has ranked it No.1 Engineering Institution in 2016.
 </p>`
 
-            
+}
+    iitmadras:{
+        title:' IIT-bombay',
+        heading:'ABOUT Iit',
+        content:
+        `<p>
+            Indian Institute of Technology Madras is a public engineering and research  institute  located in , Tamil Nadu and  is  one of  India's  most  prestigious  universities. It is recognised as an Institute of  National  Importance  by the Government of India. Founded in 1959 with  technical  and    assistance   the former  government  of  West Germany, it was  the   Indian  Institute of Technology that was established by the  Government of India  through an  Act of Parliament, to provide  education and  research facilities in engineering and technology.
+        </p>
+        <p>
+            IIT Madras  is a residential  institute  that occupies a 2.5 square kilometers  campus that  was formerly part  of the adjoining Guindy National Park. The institute has nearly 550 faculty, 8,000 students and 1,250 administrative and supporting staff. Growing ever since it obtained its charter from the Indian Parliament in 1961, much of the campus is a protected forest, carved out of the Guindy National Park, home to large numbers of chital (spotted deer), black buck, monkeys, and other rare wildlife. A natural lake, deepened in 1988 and 2003, drains most of its rainwater.
+
+        </p>
+        <p>
+         Internationally, IIT Madras was ranked #254 in the QS World University Rankings of 2015, 56 in the QS Asian University Rankings and 20 in the QS BRICS University Rankings of 2015.
+        <p>
+            The Times Higher Education World Rankings ranked it at 78 and 44 in Asia and BRICS countries respectively in 2015.
+         </p>
+         <p>
+            In India, National Institutional Ranking Framework has ranked it No.1 Engineering Institution in 2016.
+</p>`
+
 }
 function createTemplate (data) {
     var title=data.title;
@@ -67,8 +87,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/iit-madras', function (req,res) {
-  res.send(createTemplate(articleone));
+app.get('/iitname', function (req,res) {
+  res.send(createTemplate(iit[iitname]));
 });
 
 app.get('/ui/style.css', function (req, res) {
