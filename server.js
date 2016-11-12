ff4ac7f613af9e3a672f43d85f32530e0a453966
saikiran949 <saikiran9497@gmail.com>
2016-11-12 17:09:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var articles={
          <p>
             In India, National Institutional Ranking Framework has ranked it No.1 Engineering Institution in 2016.
 </p>`
-
+}
 };
 function createTemplate (data) {
     var title=data.title;
