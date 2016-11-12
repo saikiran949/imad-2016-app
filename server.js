4bdf11eaa3fc71d4f79c7089f9de5fcba850d7e4
saikiran949 <saikiran9497@gmail.com>
2016-11-12 22:17:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-    'iit-madras':{
+    'iitmadras':{
         title:' IIT-MADRAS',
         heading:'ABOUT IIT-MADRAS',
         content:
