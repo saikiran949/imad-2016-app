d4dce28130021b9bcdd26a9230dbd4e1baab93e2
saikiran949 <saikiran9497@gmail.com>
2016-11-24 12:07:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined
+  <a href="/">iitmadras</a>
 
 var articles={
     'iitmadras':{
